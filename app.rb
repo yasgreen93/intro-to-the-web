d11fc456809c20f877af75ec21682021e1437961
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-14 16:10:54 +0000

    Added html cat

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ get '/name' do
   "Rhiannon and Yasmin"
 end
 
-get '/somethingelse' do
-  "other"
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+      <img src='http://bit.ly/1eze8aE'>
+   <div>"
 end

0318342e0d34eb041e04d8549eee855b6c8d057a
Gabrielle <gabivk@gmail.com>
2016-03-15 14:23:35 +0000

    added post request

--- app.rb
+++ app.rb
@@ -10,8 +10,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end

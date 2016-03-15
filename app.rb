369df9d1d59d68fdb0817d761d9e8fa478ecb6b4
Gabrielle <gabivk@gmail.com>
2016-03-15 11:48:27 +0000

    Added Cat

--- app.rb
+++ app.rb
@@ -1,15 +1,12 @@
 #!/usr/bin/env ruby
 require 'sinatra'
-require 'shotgun'
 
 get '/' do
   'hello!'
 end
 
-get '/secret' do
-  'Top secret'
-end
-
-get '/bob' do
-  'bib'
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end

cfb77340c37a1d403458a41045c120c506d4fdbe
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-15 11:53:15 +0000

    added cat gif website

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ end
 
 get '/cat' do
   "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
+    <a href='http://procatinator.com/'>
+    <img src='http://bit.ly/1eze8aE'></a>
   </div>"
 end

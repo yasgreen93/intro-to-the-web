edeb95216de4d03f1fd1da439b0af37a8fa10fd0
Gabrielle <gabivk@gmail.com>
2016-03-15 12:39:29 +0000

    added params

--- app.rb
+++ app.rb
@@ -5,7 +5,13 @@ get '/' do
   'hello!'
 end
 
-get '/cat' do
-  @name = ['Amigo', 'Oscar', 'Viking'].sample
+get '/random-cat' do
+  @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
